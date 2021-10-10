86d02ce9f6b91f8fd311d7b30fb80e83f1548271
Andrezza Souza <andrezzasouza@id.uff.br>
2021-10-09 21:55:20 -0300

    Configure post method for /participants route

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -11,4 +11,21 @@ app.get("/participants", (req, res) => {
   res.send(participants);
 })
 
+app.post("/participants", (req, res) => {
+  console.log(req.body.name)
+  const newUsername = req.body.name;
+  const inUse = participants.find(e => e.name === newUsername)
+
+  if (newUsername !== "" && inUse === undefined && newUsername !== null) {
+    participants.push({
+      name: newUsername,
+      lastStatus: Date.now()
+    })
+    res.status(200);
+  } else {
+    res.status(400);
+  }
+  res.send()
+})
+
 app.listen(4000);
